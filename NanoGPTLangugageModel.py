4364818dbab5209c669de1316cb8ec796ee5e838
Ankur Mishra <2018ankurm@gmail.com>
2024-01-30 15:37:15 -0500

    no longer need Head class

--- NanoGPTLangugageModel.py
+++ NanoGPTLangugageModel.py
@@ -57,44 +57,6 @@ def estimate_loss() -> dict:
     model.train()
     return out
 
-class Head(nn.Module):
-    """one head of self attention"""
-
-    def __init__(self, head_size: int) -> None:
-        super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # define tril as a buffer so it is not a parameter of the model
-        self.dropout = nn.Dropout(dropout)
-
-    def forward (self, x: torch.Tensor) -> torch.Tensor:
-        """
-        performs a forward pass of the model
-
-        Parameters:
-        - x: a [B, T, C] tensor of floats representing the input sequence
-
-        Returns:
-        - out: a [B, T, C] tensor of floats representing the output sequence
-        """
-        # compute the keys, queries and values
-        B, T, C = x.shape
-        k = self.key(x) # [B, T, C]
-        q = self.query(x) # [B, T, C]
-
-        # computing the affiniities aka the attention scores
-        # (C**-0.5) is a scaling factor to normalize the dot product
-        wei = q @ k.transpose(-2, -1) * (C**-0.5) # [B, T, C] @ [B, C, T] -> [B, T, T]
-        # decoder block
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # [B, T, T]
-        wei = F.softmax(wei, dim=-1) # [B, T, T]
-        wei = self.dropout(wei)
-        # perform the weight aggregation of vals
-        v = self.value(x) # [B, T, C]
-        out = wei @ v # [B, T, T] @ [B, T, C] -> [B, T, C]
-        return out
-
 class MultiHeadAttention(nn.Module):
     """multiple heads of self attention in parallel"""
     def __init__(self, num_heads:int, head_size:int) -> None:
