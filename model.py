26f624cabd950595e0c268490c914db398dfaec3
Ankur Mishra <2018ankurm@gmail.com>
2024-03-03 20:14:13 -0500

    seperate out layers and scale for ln

--- model.py
+++ model.py
@@ -38,26 +38,35 @@ class GptLanguageModel (nn.Module):
         super().__init__()
         for k, v in hyperparameters.__dict__.items():
             setattr(self, k, v)
-        self.token_emb_table = nn.Parameter(torch.randn((self.vocab_size, self.n_embd)))
-        self.position_emb_table = nn.Parameter(torch.randn((self.block_size, self.n_embd)))
+        self.token_emb_table = nn.Parameter(
+            torch.randn((self.vocab_size, self.n_embd)))
+        self.position_emb_table = nn.Parameter(
+            torch.randn((self.block_size, self.n_embd)))
 
         # MLP projection matrices
-        self.w_in = nn.Parameter(torch.randn((self.n_embd, 4 * self.n_embd)) / self.n_embd ** 0.5)
-        self.w_out = nn.Parameter(torch.randn((4 * self.n_embd, self.n_embd)) / (4 * self.n_embd) ** 0.5)
+
+        self.w_in = nn.Parameter(torch.randn(
+            (self.n_layer, self.n_embd, 4 * self.n_embd)) / self.n_embd ** 0.5)
+        self.w_out = nn.Parameter(torch.randn(
+            (self.n_layer, 4 * self.n_embd, self.n_embd)) / (4 * self.n_embd) ** 0.5)
 
         # projection matrices for attention
         self.head_dim = self.n_embd // self.n_head
 
-        self.attention_k = nn.Parameter(torch.randn((self.n_embd, self.n_head, self.head_dim)) / self.head_dim ** 0.5)  # [C, h, d]
-        self.attention_q = nn.Parameter(torch.randn((self.n_embd, self.n_head, self.head_dim)) / self.head_dim ** 0.5)
-        self.attention_v = nn.Parameter(torch.randn((self.n_embd, self.n_head, self.head_dim)) / self.head_dim ** 0.5)
+        self.attention_kvq = nn.Parameter(torch.randn(
+            (self.n_layer, 3, self.n_embd, self.n_head, self.head_dim)) / self.head_dim ** 0.5)  # [L, 3, C, h, d]
 
         # for communication between attention heads
-        self.out_proj = nn.Parameter(torch.randn((self.n_head, self.head_dim, self.n_embd)) / self.head_dim ** 0.5)  # [h, d, C]
+        self.out_proj = nn.Parameter(torch.randn(
+            (self.n_layer, self.n_head, self.head_dim, self.n_embd)) / self.head_dim ** 0.5)  # [h, d, C]
 
-        self.register_buffer('tril', torch.tril(torch.ones(1, 1, self.block_size, self.block_size)))
+        self.register_buffer('tril', torch.tril(
+            torch.ones(1, 1, self.block_size, self.block_size)))
 
-        self.lm_head = nn.Parameter(torch.randn((self.n_embd, self.vocab_size)) / self.n_embd ** 0.5)
+        self.lm_head = nn.Parameter(torch.randn(
+            (self.n_embd, self.vocab_size)) / self.n_embd ** 0.5)
+
+        self.scale = nn.Parameter(torch.randn(self.n_layer, 3, self.n_embd))
 
     def forward(self, idx: torch.Tensor, targets: Optional[torch.Tensor] = None, blocks_kvcache: Optional[BlocksKVCacheType] = None) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[BlocksKVCacheType]]:
         """
@@ -69,17 +78,14 @@ class GptLanguageModel (nn.Module):
 
         history_length = 0 if not blocks_kvcache or not blocks_kvcache[
             0] else blocks_kvcache[0][0].shape[2]
-        pos_emb = self.position_emb_table[torch.arange(T, device=self.device) + history_length]
-
+        pos_emb = self.position_emb_table[torch.arange(
+            T, device=self.device) + history_length]
 
         x = tok_emb + pos_emb  # [B, T, C]
 
-        for layer in range(self.n_layer):
-            x = F.layer_norm(x, (B, T, C))
+        for layer, (projections, layer_w_in, layer_w_out, mha_out, layer_scale) in enumerate(zip(self.attention_kvq, self.w_in, self.w_out, self.out_proj, self.scale)):
+            x = F.layer_norm(x, [C], weight=layer_scale[0])
             kv_cache = blocks_kvcache[layer] if blocks_kvcache else None
-
-            projections = torch.stack(
-                [self.attention_k, self.attention_v, self.attention_q], dim=0)  # [3, C, h, d]
 
             # [B, T, C] @ [3, C, h, d] -> [3, B, h, T, d], S = 3
             k, v, q = torch.einsum('btc,schd->sbhtd', x, projections)
@@ -112,23 +118,24 @@ class GptLanguageModel (nn.Module):
             out = torch.einsum('bhqk,bhkd->bhqd', att_wei, v)
 
             # [B, h, Q, d] @ [h, d, C] -> [B, Q, C]
-            out = torch.einsum('bhqd,hdc->bqc', out, self.out_proj)
+            out = torch.einsum('bhqd,hdc->bqc', out, mha_out)
             out = F.dropout(out, p=self.dropout, training=self.training)
 
-            x = F.layer_norm(x + out, (B, T, C))
-            x = x + out  # residual connection
+            out = F.layer_norm(out, [C], weight=layer_scale[1])
+            x = x + out
 
             # MLP block
             # [B, T, C] @ [C, 4C] -> [B, T, 4C]
-            mlp_hidden = torch.einsum('btc,cd->btd', x, self.w_in)
+            mlp_hidden = torch.einsum('btc,cd->btd', x, layer_w_in)
             mlp_hidden = F.relu(mlp_hidden)
 
             # [B, T, 4C] @ [4C, C] -> [B, T, C]
-            mlp_out = torch.einsum('btd,dc->btc', mlp_hidden, self.w_out)
+            mlp_out = torch.einsum('btd,dc->btc', mlp_hidden, layer_w_out)
             mlp_out = F.dropout(x, p=self.dropout, training=self.training)
             x = x + mlp_out  # residual connection
 
-        x = F.layer_norm(x, (B, T, C))
+        x = F.layer_norm(x, [C], weight=layer_scale[2])
+
         logits = torch.einsum('btc,cd->btd', x, self.lm_head)
         loss = None
 
--- train.py
+++ train.py
@@ -10,26 +10,32 @@ from typing import Tuple
 
 
 low_power_hyperparameters = GptConfig(
-    batch_size = 32,
-    block_size = 8,
-    max_epochs = 5000,
-    eval_interval = 500,
-    learning_rate = 1e-3,
-    eval_iters = 200,
-    n_embd = 32,
-    n_layer = 3,
-    n_head = 4,
-    dropout = 0.2
+    batch_size=32,
+    block_size=8,
+    max_epochs=5000,
+    eval_interval=500,
+    learning_rate=1e-3,
+    eval_iters=200,
+    n_embd=32,
+    n_layer=3,
+    n_head=4,
+    dropout=0.2
 )
 hyperparameters = GptConfig()
 
+
 def get_batch(split: str) -> Tuple[torch.Tensor, torch.Tensor]:
     data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - hyperparameters.block_size, size=(hyperparameters.batch_size,)) # will return batch_size random numbers that are offsets of the data set 
-    x = torch.stack([data[i:i+hyperparameters.block_size] for i in ix]) # builds a stack of tensors of size blocksize for each random number in ix
-    y = torch.stack([data[i+1:i+hyperparameters.block_size+1] for i in ix]) # offset by 1 stack of tensors
+    # will return batch_size random numbers that are offsets of the data set
+    ix = torch.randint(len(data) - hyperparameters.block_size,
+                       size=(hyperparameters.batch_size,))
+    # builds a stack of tensors of size blocksize for each random number in ix
+    x = torch.stack([data[i:i+hyperparameters.block_size] for i in ix])
+    y = torch.stack([data[i+1:i+hyperparameters.block_size+1]
+                    for i in ix])  # offset by 1 stack of tensors
     x, y = x.to(hyperparameters.device), y.to(hyperparameters.device)
     return x, y
+
 
 @torch.no_grad()
 def estimate_loss(model) -> dict:
@@ -45,8 +51,9 @@ def estimate_loss(model) -> dict:
     model.train()
     return out
 
+
 if __name__ == "__main__":
-    
+
     # Get the current date and time
     current_date_time = datetime.now()
 
@@ -57,6 +64,7 @@ if __name__ == "__main__":
 
     task = Task.init(project_name='nanogpt', task_name=formatted_date_time)
     task.execute_remotely('default', clone=False, exit_process=True)
+    logger = task.get_logger()
 
     model = GptLanguageModel(hyperparameters)
     m = model.to(hyperparameters.device)
@@ -69,23 +77,27 @@ if __name__ == "__main__":
         # every once in a while eval loss on train and val sets
         if steps % model.eval_interval == 0:
             losses = estimate_loss(m)
-            print(f"Step: {steps}, Train loss: {losses['train']:.2f}, Val loss: {losses['val']:.2f}")   
+            print(
+                f"Step: {steps}, Train loss: {losses['train']:.2f}, Val loss: {losses['val']:.2f}")
         # sample a batch of data
         xb, yb = get_batch('train')
 
         # evaluate the loss
         logits, loss, _ = m(xb, yb)
 
-        task.set_last_metrics({'loss': loss.item()}, plot_name='loss', series_name='loss')
+        logger.report_scalar(title="Train Loss", series="Train Loss",
+                             value=loss.item(), iteration=steps)
 
-        optimizer.zero_grad(set_to_none=True) # clear the gradients
-        loss.backward() # compute gradients
-        optimizer.step() # update parameters
+        optimizer.zero_grad(set_to_none=True)  # clear the gradients
+        loss.backward()  # compute gradients
+        optimizer.step()  # update parameters
 
     print(loss.item())
 
     torch.save(m.state_dict(), 'model_weights.pth')
 
     start_str = "\n"
-    idx = torch.tensor(encode(start_str), dtype=torch.long, device=hyperparameters.device).unsqueeze(0)
-    print(decode(m.generate(idx = idx, max_new_tokens=hyperparameters.block_size)[0].tolist()))
+    idx = torch.tensor(encode(start_str), dtype=torch.long,
+                       device=hyperparameters.device).unsqueeze(0)
+    print(decode(m.generate(
+        idx=idx, max_new_tokens=hyperparameters.block_size)[0].tolist()))
