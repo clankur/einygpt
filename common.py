61b34c248e07e2cd02e8a6166a5d1e9b4243b5fe
Ankur Mishra <2018ankurm@gmail.com>
2024-05-01 23:55:01 -0400

    use new dataloader

--- common.py
+++ common.py
@@ -1,11 +1,10 @@
 import torch
 from dataclasses import dataclass
-from typing import Iterator, List, Dict, Tuple, Optional
-import itertools
-from types import SimpleNamespace
+from typing import List, Dict, Tuple, Optional
 from datasets import load_dataset
 from transformers import AutoTokenizer, PreTrainedTokenizer
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
+import functools
 
 KVCacheType = Tuple[torch.Tensor, torch.Tensor]
 BlocksKVCacheType = List[Optional[KVCacheType]]
@@ -15,9 +14,9 @@ class GptConfig:
     """hyperparameters for GptLanguageModel"""
 
     batch_size: int = 64
-    block_size: int = 128
+    block_size: int = 256
     max_epochs: int = 5000
-    learning_rate: float = 3e-5
+    learning_rate: float = 3e-4
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
     n_embd: int = 384
     n_head: int = 6
@@ -25,125 +24,63 @@ class GptConfig:
     n_layer: int = 6
     dropout: float = 0.2
     seed: int = 42
-    warmup_steps: int = 1000
-    
+    warmup_steps: int = 500
+    tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
+    vocab_size: int = tokenizer.vocab_size
+
 hyperparameters = GptConfig()
 
-class Collator:
-    def __init__ (self, tokenizer: PreTrainedTokenizer):
-        self.tokenizer = tokenizer
+class TinyStoriesLoader:
 
-    def __call__(self, examples: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
-        batch = self.tokenizer.pad(examples, return_attention_mask=False, return_tensors='pt')
+    def __init__(self, config: GptConfig) -> None:
+        batch_size, max_length, num_workers = config.batch_size, config.block_size, 0
+        self.tokenizer = config.tokenizer
+
+        tokenize = functools.partial(
+            self.tokenizer, 
+            padding="max_length",
+            truncation=True, 
+            max_length=max_length,
+            add_special_tokens=True,
+            return_attention_mask=False,
+            return_token_type_ids=False,
+            return_tensors="pt"
+        )
+
+        dataset = load_dataset("roneneldan/TinyStories", streaming=True, split='train')
+
+        tokenized = dataset.map(tokenize, batched=True, input_columns=['text'], remove_columns=["text"])
+
+        self.dataloader = DataLoader(
+            tokenized,
+            num_workers=num_workers,
+            collate_fn=self.collate,
+            drop_last=True,
+            batch_size=batch_size
+        )
+        self.iterator = iter(self.dataloader)
+    
+    def collate(self, batch: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        max_len = max(len(item['input_ids']) for item in batch)
+
+        # Pad to the right manually
+        padded_input_ids = [torch.cat([
+            item['input_ids'],
+            torch.full((max_len - len(item['input_ids']),), self.tokenizer.pad_token_id, dtype=torch.long)
+        ]) for item in batch]
+
+        input_ids = torch.stack(padded_input_ids)
 
         return {
-            "inputs": batch['input_ids'][:, :-1],
-            "inputs_position": batch['position'][:, :-1],
-            "inputs_sequence": batch['sequences'][:, :-1],
-            "targets": batch['input_ids'][:, 1:],
-            "targets_position": batch['position'][:, 1:],
-            "targets_sequence": batch['sequences'][:, 1:]
+            'input_ids': input_ids
         }
-    
-def package_data(dataset: Dataset, tokenizer: PreTrainedTokenizer, config: GptConfig):
-    """
-    Tokenize the dataset and package it into chunks of block_size
-    dataset: dataset to be tokenized
-    tokenizer: tokenizer to be applied
-    config: hyperparameters for model
-    """
 
-    def tokenize(examples: Dict[str, str]) -> PreTrainedTokenizer:
-        return tokenizer(
-            examples['text'],
-            padding=False, # we will pad with the next sequence
-            truncation=False, 
-            max_length=None, 
-            add_special_tokens=False,
-            return_token_type_ids=False, # ?
-            return_attention_mask=False,
-        )
-    
-    def prep_example (examples: List[Dict[str, torch.Tensor]]) -> Iterator[torch.Tensor]:
-        for example in examples:
-            yield tokenizer.bos_token_id
-            yield from example
-    
-    def new_chunk() -> SimpleNamespace:
-        """
-        Create a chunk - a SimpleNamespace with the following attributes:
-            ids: token ids of the chunk
-            positions: position embeddings of the chunk
-            sequences: all sequences of the chunk storing the idx of each sequence
-            sequence_idx: current sequence idx
-            position: current position
-        """
+    def __iter__(self):
+        return self
 
-        return SimpleNamespace(
-            ids = [], 
-            positions = [], 
-            sequences=[], 
-            sequence_idx=0, 
-            position=0
-        )
-
-    def chunk(examples: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        """
-        Package the tokenized examples into chunks of block_size adding the position and sequence embeddings
-        and changing the input_ids to a list of lists each containing block_size tokens
-        """
-
-        chunk = new_chunk()
-        chunk_len = config.block_size
-        global_ids = []
-        global_positions = []
-        global_sequences = []
-
-        for token_id in prep_example(examples['input_ids']):
-            if token_id == tokenizer.bos_token_id:
-                chunk.sequence_idx += 1
-                chunk.position = 0
-            chunk.ids.append(token_id)
-            chunk.positions.append(chunk.position)
-            chunk.sequences.append(chunk.sequence_idx) 
-            chunk.position += 1
-
-            if len(chunk.ids) == chunk_len:
-                global_ids.append(chunk.ids)
-                global_positions.append(chunk.positions)
-                global_sequences.append(chunk.sequences)
-                chunk = new_chunk()
-        
-        if not global_ids: # we didn't package any chunks
-            return []
-        
-        examples['input_ids'] = global_ids
-        examples['position'] = global_positions
-        examples['sequences'] = global_sequences
-
-        return examples
-
-
-    return dataset.map(
-        tokenize, 
-        batched=True, 
-        remove_columns=['text']
-    ).map(chunk, batched=True, batch_size=config.batch_size)
-
-def make_iterator(config: GptConfig, dataset: Dataset, tokenizer: PreTrainedTokenizer) -> Iterator[Tuple[torch.Tensor, torch.Tensor]]:
-    batch_size = config.batch_size
-    og_dataset = dataset.shuffle(config.seed)
-    for epoch in itertools.count():
-        rdm_dataset = og_dataset.shuffle(config.seed + epoch)
-        rdm_dataset = package_data(rdm_dataset, tokenizer, config)
-        dataloader = iter(DataLoader(rdm_dataset, batch_size=batch_size, collate_fn=Collator(tokenizer)))
-        for data in dataloader:
-            yield data['inputs'], data['targets']
-
-def get_iterator_tokenizer_and_config (config: GptConfig) -> Tuple[Iterator, PreTrainedTokenizer]:
-    tokenizer = AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
-    dataset = load_dataset("roneneldan/TinyStories", streaming=True, split='train')
-    iterator = make_iterator(config, dataset, tokenizer)
-    config.vocab_size = tokenizer.vocab_size
-
-    return iterator, tokenizer, config
+    def __next__(self):
+        try:
+            return next(self.iterator)
+        except StopIteration:
+            self.iterator = iter(self.dataloader)
+            raise StopIteration
--- train.py
+++ train.py
@@ -6,10 +6,10 @@ from typing import Tuple, List, Dict
 from collections import OrderedDict
 from einops import rearrange
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from common import hyperparameters, get_iterator_tokenizer_and_config
+from common import hyperparameters, TinyStoriesLoader
 from model import GptLanguageModel
 
-remote = True
+remote = False
 
 if __name__ == "__main__":
 
@@ -26,40 +26,44 @@ if __name__ == "__main__":
         task.execute_remotely('default', clone=False, exit_process=True)
     logger = task.get_logger()
 
-    data_iterator, tokenizer, hyperparameters = get_iterator_tokenizer_and_config(hyperparameters)
+    dataloader = TinyStoriesLoader(hyperparameters)
     einops_model = GptLanguageModel(hyperparameters)
+    tokenizer = hyperparameters.tokenizer
     m = einops_model.to(einops_model.device)
 
     # register hook
-    # register_hook(m)
-    # use a torch scheduler
-    # linear warm up and cosine decay
-    # try setting up warm up and decay for learning rate
+    # register_hook(m)  
 
     # create a pytorch optimizer and scheduler
     optimizer = torch.optim.AdamW(m.parameters(), lr=m.learning_rate)
-    scheduler = CosineAnnealingLR(optimizer, T_max=m.max_epochs, eta_min=1e-6)
+    scheduler = CosineAnnealingLR(optimizer, T_max=m.max_epochs, eta_min=1e-5)
 
     # training the model
     for steps in range(m.max_epochs):
         # sample a batch of data
-        xb, yb = next(data_iterator)
-
-        # evaluate the loss
-        logits, loss, _ = m(xb.to(m.device), yb.to(m.device))
-
-        logger.report_scalar(title="Train Loss", series="Train Loss",
-                             value=loss.item(), iteration=steps)
-
-        optimizer.zero_grad(set_to_none=True)  # clear the gradients
-        loss.backward()  # compute gradients
-        optimizer.step()  # update parameters
+        inputs = next(dataloader)
+        xb, yb = inputs['input_ids'][:, :-1], inputs['input_ids'][:, 1:]
 
         # apply linear warm up for learning rate
         if steps < m.warmup_steps:
             lr_scale = min(1.0, float(steps + 1) / m.warmup_steps)
             for group in optimizer.param_groups:
                 group['lr'] = m.learning_rate * lr_scale
+            logger.report_scalar(title="LR Scale", series="LR Scale",
+                                value=lr_scale, iteration=steps)
+
+        # evaluate the loss
+        logits, loss, _ = m(xb.to(m.device), yb.to(m.device))
+        
+        logger.report_scalar(title="Train Loss", series="Train Loss",
+                             value=loss.item(), iteration=steps)
+        logger.report_scalar(title="Learning Rate", series="Learning Rate",
+                                value=optimizer.param_groups[0]['lr'], iteration=steps)
+
+        optimizer.zero_grad(set_to_none=True)  # clear the gradients
+
+        loss.backward()  # compute gradients
+        optimizer.step()  # update parameters
 
         # apply cosine decay for learning rate
         scheduler.step()
@@ -67,7 +71,7 @@ if __name__ == "__main__":
     print(loss.item())
 
     torch.save(m.state_dict(), 'model_weights.pth')
-
+ 
     start_str = "\n"
     curr_token = tokenizer.encode(start_str)
     idx = torch.tensor(curr_token, dtype=torch.long,
