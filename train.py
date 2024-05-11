f0dbb4343eae119f4828bac4de66838f804a0ba5
Ankur Mishra <2018ankurm@gmail.com>
2024-05-11 18:34:05 -0400

    update train to log metrics at cadance of 100 steps

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 from common import hyperparameters, TinyStoriesLoader
 from model import GptLanguageModel
 
-remote = False
+remote = True
 load_last_checkpoint = False
 
 if __name__ == "__main__":
@@ -13,12 +13,12 @@ if __name__ == "__main__":
     # Get the current date and time
     current_date_time = datetime.now()
 
-    # Format the date and time in a string
     formatted_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    task = Task.init(project_name='nanogpt', task_name=formatted_date_time)
+    task = Task.init(project_name='einygpt', task_name=formatted_date_time)
     if remote:
         task.execute_remotely('default', clone=False, exit_process=True)
+
     logger = task.get_logger()
     task.connect(vars(hyperparameters))
     dataloader = TinyStoriesLoader(hyperparameters, split='train')
@@ -36,14 +36,13 @@ if __name__ == "__main__":
     # create a pytorch optimizer and scheduler
     optimizer = torch.optim.AdamW(m.parameters(), lr=m.learning_rate)
     scheduler = CosineAnnealingLR(optimizer, T_max=m.max_steps, eta_min=m.learning_rate * .1)
-    # training the model
 
     epochs = 0
     for steps in range(m.max_steps):
-        # sample a batch of data
         try:
             inputs = next(dataloader)
         except StopIteration:
+            torch.save(m.state_dict(), f'model_intermediate_weights.pth')
             epochs += 1
             hyperparameters.seed += 1
             dataloader = TinyStoriesLoader(hyperparameters, split='train')
@@ -51,24 +50,22 @@ if __name__ == "__main__":
 
         xb, yb = inputs['input_ids'][:, :-1], inputs['input_ids'][:, 1:]
 
-        # switch to chain schedulers after this
         # apply linear warm up for learning rate
         if steps < m.warmup_steps:
             lr_scale = min(1.0, float(steps + 1) / m.warmup_steps)
             for group in optimizer.param_groups:
                 group['lr'] = m.learning_rate * lr_scale
-            logger.report_scalar(title="LR Scale", series="LR Scale",
-                                value=lr_scale, iteration=steps)
 
         # evaluate the loss
         logits, loss, _ = m(xb.to(m.device), yb.to(m.device))
         
-        logger.report_scalar(title="Train Loss", series="Train Loss",
-                             value=loss.item(), iteration=steps)
-        logger.report_scalar(title="Learning Rate", series="Learning Rate",
-                                value=optimizer.param_groups[0]['lr'], iteration=steps)
-        logger.report_scalar(title="Epochs", series="Epochs",
-                                value=epochs, iteration=steps)
+        if steps % 100 == 0:  
+            logger.report_scalar(title="Train Loss", series="Train Loss",
+                                value=loss.item(), iteration=steps)
+            logger.report_scalar(title="Learning Rate", series="Learning Rate",
+                                    value=optimizer.param_groups[0]['lr'], iteration=steps)
+            logger.report_scalar(title="Epochs", series="Epochs",
+                                    value=epochs, iteration=steps)
         
         optimizer.zero_grad(set_to_none=True)  # clear the gradients
 
@@ -79,8 +76,11 @@ if __name__ == "__main__":
         scheduler.step()
 
     print(loss.item())
+    training_end_time = datetime.now()
+    print("Time taken for training: ", str(training_end_time - current_date_time))
 
     torch.save(m.state_dict(), 'model_weights.pth')
+
     start_str = "\n"
     curr_token = tokenizer.encode(start_str)
     idx = torch.tensor(curr_token, dtype=torch.long,
